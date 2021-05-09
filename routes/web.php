d6663baf538c801ae719675258efa5d1e26926c9
Enes Karacaer <eneskaracaer@hotmail.com>
2021-05-09 21:50:29 +0300

    selenium test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CalcController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CalcController::class, 'home']);
+
+Route::post('/calc', [CalcController::class, 'calc']);
